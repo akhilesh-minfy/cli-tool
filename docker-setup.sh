452e488ef860ea915a56cbfcd24753bf32d2f228
akhilesh-minfy <akhilesh.gone@minfytech.com>
2025-07-17 19:22:10 +0530

    Update docker-setup.sh

--- docker-setup.sh
+++ docker-setup.sh
@@ -9,3 +9,4 @@ sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-a
  curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
  curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
  sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+ curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
